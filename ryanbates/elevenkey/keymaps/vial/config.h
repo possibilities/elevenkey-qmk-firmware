11756eae947ff790f0fdcd90bef7c24d40ff9df6
Mike Bannister <notimpossiblemike@gmail.com>
2022-03-29 21:21:44 -0400

    Add vial support

---
+++ ryanbates/elevenkey/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x56, 0x46, 0x06, 0x0E, 0xB0, 0x92, 0xA4, 0xB5}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 4, 0 }
---
+++ ryanbates/elevenkey/keymaps/vial/keymap.c
@@ -0,0 +1,76 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* LAYER 0
+ * ,-------------------------------.
+ * |  L1   | Prev  | Next  | Play  |
+ * |-------+-------+-------+-------|
+ * |       |       |       |       |
+ * |-------+-------+-------+-------|
+ * |       |       |       |       |
+ * |-------+-------+-------+-------|
+ * |       |       |       |       |
+ * `-------------------------------'
+ */
+[0] = LAYOUT(
+  TG(1),       KC_MPRV,    KC_MNXT,    KC_MPLY,
+  KC_NO,       KC_NO,      KC_NO,      KC_NO,
+  KC_NO,       KC_NO,      KC_NO,      KC_NO,
+  KC_NO,       KC_NO,      KC_NO,      KC_NO
+),
+/* LAYER 2-4
+ * ,-------------------------------.
+ * |       |       |       |       |
+ * |-------+-------+-------+-------|
+ * |       |       |       |       |
+ * |-------+-------+-------+-------|
+ * |       |       |       |       |
+ * |-------+-------+-------+-------|
+ * |       |       |       |       |
+ * `-------------------------------'
+ */
+[1] = LAYOUT(
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS,
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS,
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS,
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS
+),
+[2] = LAYOUT(
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS,
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS,
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS,
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS
+),
+[3] = LAYOUT(
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS,
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS,
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS,
+  KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS
+)
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (get_highest_layer(layer_state|default_layer_state) > 0) {
+          if (clockwise) {
+              tap_code_delay(KC_MS_WH_RIGHT, 10);
+          } else {
+              tap_code_delay(KC_MS_WH_LEFT, 10);
+          }
+        } else {
+          if (clockwise) {
+              tap_code_delay(KC_MS_WH_DOWN, 10);
+          } else {
+              tap_code_delay(KC_MS_WH_UP, 10);
+          }
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    }
+    return false;
+};
