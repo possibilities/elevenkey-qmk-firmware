299b82fef85caab1631440598ead276cc881237c
Mike Bannister <notimpossiblemike@gmail.com>
2022-03-29 22:22:54 -0400

    Vial support (#1)
    * Add empty layers for via
    
    * Disable qmk lighting
    
    * Add vial support
    
    * Add vial encoder support

--- ryanbates/elevenkey/keymaps/vial/keymap.c
+++ ryanbates/elevenkey/keymaps/vial/keymap.c
@@ -49,28 +49,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,       KC_TRNS,      KC_TRNS,      KC_TRNS
 )
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (get_highest_layer(layer_state|default_layer_state) > 0) {
-          if (clockwise) {
-              tap_code_delay(KC_MS_WH_RIGHT, 10);
-          } else {
-              tap_code_delay(KC_MS_WH_LEFT, 10);
-          }
-        } else {
-          if (clockwise) {
-              tap_code_delay(KC_MS_WH_DOWN, 10);
-          } else {
-              tap_code_delay(KC_MS_WH_UP, 10);
-          }
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code_delay(KC_VOLU, 10);
-        } else {
-            tap_code_delay(KC_VOLD, 10);
-        }
-    }
-    return false;
-};
